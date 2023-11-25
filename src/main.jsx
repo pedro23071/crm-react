45c8dad4be54fcea02fb0e7414e24237e7769943
Pedro A <juan-delprado@hotmail.com>
2023-11-25 14:08:37 -0600

    instalando react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import './index.css'
 
 import Layout from './components/layout'
 import NuevoCliente from './pages/NuevoCliente'
+import Index from './pages/Index'
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
         children: [
+            {
+                index: true,
+                element: <Index />
+            },
             {
                 path: '/clientes/nuevo',
                 element: <NuevoCliente />
