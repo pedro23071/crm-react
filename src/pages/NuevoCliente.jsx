4c588d04895958f064bc3fe91ceccc991c39c458
Pedro Antonio <pedro23071@gmail.com>
2023-11-28 21:36:15 -0600

    crear formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,7 +1,44 @@
+import { useNavigate, Form } from "react-router-dom"
+import Formulario from "../components/Formulario"
+
+export const  action = async ({request}) => {
+    const formData = await request.formData();
+    const datos = Object.fromEntries(formData)
+    console.log(datos)
+    return ''
+}
+
+
 const NuevoCliente = () => {
+
+    const navigate = useNavigate()
+
     return(
         <>
-            <h1>Nuevo cliente</h1>
+            <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+            <p className="mt-3">Registra un nuevo cliente</p>
+
+            <div className="flex justify-end">
+                <button 
+                    className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+                    onClick={() => navigate(-1)}    
+                    >
+                    Volver
+                </button>
+            </div>
+
+            <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+                <Form
+                   method="POST"
+                >
+                    <Formulario />
+                    <input 
+                        type="submit" 
+                        value="Regsitrar Cliente" 
+                        className="mt-5 w-full bg-blue-800 p-3 hover:bg-blue-900 uppercase font-bold text-white text-lg"
+                        />
+                </Form>
+            </div>
         </>
     )
 }
